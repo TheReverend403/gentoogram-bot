056e0935d6e89d1e626533940a76ca6d6dd543bb
Lee Watson <rev@revthefox.co.uk>
2019-11-25 15:33:08 +0000

    Remove unnecessary code.

--- gentoogram/bot.py
+++ gentoogram/bot.py
@@ -9,16 +9,11 @@ logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 logger = logging.getLogger('bot')
 
 
-def create_updater(token):
-    updater = Updater(token=token, use_context=True)
-    return updater
-
-
 def main(args):
     token = settings['token']
-    updater = create_updater(token)
+    updater = Updater(token=token, use_context=True)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(spam_handler)
+    dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, anti_china_spam))
     updater.start_polling()
     logger.info('Ready!')
 
@@ -32,8 +27,6 @@ def anti_china_spam(update, context):
         update.chat.kick_chat_member(update.message.from_user.id)
         update.message.delete()
 
-
-spam_handler = MessageHandler(Filters.status_update.new_chat_members, anti_china_spam)
 
 if __name__ == '__main__':
     main(sys.argv)
