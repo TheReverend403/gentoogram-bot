5589c8f5ab1464bbe36cd2868e9f6de5b41ed565
Lee Watson <rev@revthefox.co.uk>
2021-01-12 17:06:19 +0000

    Add editorconfig, black and flake8.

--- gentoogram/__main__.py
+++ gentoogram/__main__.py
@@ -27,16 +27,18 @@ from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 from gentoogram import BASE_DIR
 
-config = Dynaconf(settings_file=[BASE_DIR / 'settings.yml'])
-dictConfig(config.get('logging'))
-logger = logging.getLogger('gentoogram')
-version = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('UTF-8')
+config = Dynaconf(settings_file=[BASE_DIR / "settings.yml"])
+dictConfig(config.get("logging"))
+logger = logging.getLogger("gentoogram")
+version = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode(
+    "UTF-8"
+)
 re_flags = re.UNICODE | re.IGNORECASE | re.DOTALL
 
 
 def sentry_before_send(event, hint):
-    if 'exc_info' in hint:
-        exc_type, exc_value, tb = hint['exc_info']
+    if "exc_info" in hint:
+        exc_type, exc_value, tb = hint["exc_info"]
         if isinstance(exc_value, (TelegramError, NetworkError)):
             return None
 
@@ -47,10 +49,12 @@ def admin(func):
     @wraps(func)
     def wrapped(update, context, *args, **kwargs):
         user_id = update.effective_user.id
-        if user_id not in config.get('admins', []):
-            logger.debug(f'User {user_id} was denied access to {func.__name__}')
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text='That command can only be used by bot admins. Which you are not.')
+        if user_id not in config.get("admins", []):
+            logger.debug(f"User {user_id} was denied access to {func.__name__}")
+            context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="That command can only be used by bot admins. Which you are not.",
+            )
             return
         return func(update, context, *args, **kwargs)
 
@@ -58,117 +62,120 @@ def admin(func):
 
 
 def main():
-    sentry_dsn = config.get('sentry.dsn')
+    sentry_dsn = config.get("sentry.dsn")
     if sentry_dsn:
         sentry_sdk.init(sentry_dsn, release=version, before_send=sentry_before_send)
 
-    token = config.get('telegram.token')
+    token = config.get("telegram.token")
     updater = Updater(token=token, use_context=True)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('reload', reload_config))
-    dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, chat_filter))
+    dispatcher.add_handler(CommandHandler("reload", reload_config))
+    dispatcher.add_handler(
+        MessageHandler(Filters.status_update.new_chat_members, chat_filter)
+    )
     dispatcher.add_handler(MessageHandler(Filters.text, chat_filter))
     dispatcher.add_handler(MessageHandler(Filters.forwarded, chat_filter))
     updater.start_polling()
-    logger.info('Ready!')
+    logger.info("Ready!")
 
 
 @admin
 def reload_config(update, context):
     config.reload()
-    logger.debug('Config reloaded.')
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Config reloaded!')
+    logger.debug("Config reloaded.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Config reloaded!")
 
 
 def is_spammer(user_id: int) -> bool:
     try:
-        with requests.get(f'https://api.cas.chat/check?user_id={user_id}', timeout=5) as response:
+        with requests.get(
+            f"https://api.cas.chat/check?user_id={user_id}", timeout=5
+        ) as response:
             check_result = response.json()
     except (requests.RequestException, TimeoutError) as exc:
         logger.warning(exc)
         return False
 
-    if check_result.get('ok'):
-        offenses = check_result.get('result').get('offenses')
-        if offenses >= config.get('antispam_threshold', 1):
-            logger.info(f'User {user_id} failed CAS spam check with {offenses} offense(s).')
+    if check_result.get("ok"):
+        offenses = check_result.get("result").get("offenses")
+        if offenses >= config.get("antispam_threshold", 1):
+            logger.info(
+                f"User {user_id} failed CAS spam check with {offenses} offense(s)."
+            )
             return True
 
     return False
 
 
-def chat_filter(update, context):
-    logger.debug(f'{update}')
+def chat_filter(update, context):  # noqa: C901
+    logger.debug(f"{update}")
 
     message = update.effective_message
     if not message:
         return
 
     chat = update.effective_chat
-    if chat.id not in [int(chat) for chat in config.get('telegram.chats', [])]:
+    if chat.id not in [int(chat) for chat in config.get("telegram.chats", [])]:
         return
 
     user = update.effective_user
-    username = user.username if user.username else ''
+    username = user.username if user.username else ""
 
-    full_name = f'{user.first_name}'
+    full_name = f"{user.first_name}"
     if user.last_name:
-        full_name += f' {user.last_name}'
+        full_name += f" {user.last_name}"
 
     log_data = {
-        'user': {
-            'id': user.id,
-            'username': username,
-            'first_name': user.first_name,
-            'last_name': user.last_name
+        "user": {
+            "id": user.id,
+            "username": username,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
         },
-        'chat': {
-            'id': chat.id,
-            'type': chat.type,
-            'username': chat.username
-        }
+        "chat": {"id": chat.id, "type": chat.type, "username": chat.username},
     }
 
-    filters = config.get('filters')
-    for pattern in filters.get('usernames'):
-        if re.search(pattern, full_name, re_flags) or re.search(pattern, username, re_flags):
-            log_data.update({'regex': pattern})
-            logger.info(f'Username filter match: {log_data}')
+    filters = config.get("filters")
+    for pattern in filters.get("usernames"):
+        if re.search(pattern, full_name, re_flags) or re.search(
+            pattern, username, re_flags
+        ):
+            log_data.update({"regex": pattern})
+            logger.info(f"Username filter match: {log_data}")
 
             if chat.kick_member(user.id) and message.delete():
-                logger.info(f'Kicked user {user.id}.')
+                logger.info(f"Kicked user {user.id}.")
             else:
-                logger.info(f'Could not kick user {user.id}.')
+                logger.info(f"Could not kick user {user.id}.")
 
             break
 
     # This is a new chat member event.
     if not message.text:
         if is_spammer(user.id):
-            logger.info(f'CAS check match: {log_data}')
+            logger.info(f"CAS check match: {log_data}")
             if chat.kick_member(user.id) and message.delete():
-                logger.info(f'Kicked user {user.id} (CAS).')
+                logger.info(f"Kicked user {user.id} (CAS).")
             else:
-                logger.info(f'Could not kick user {user.id} (CAS).')
+                logger.info(f"Could not kick user {user.id} (CAS).")
             return
         return
 
-    for pattern in filters.get('messages'):
+    for pattern in filters.get("messages"):
         if re.search(pattern, message.text, re_flags):
-            log_data.update({
-                'message': {
-                    'id': message.message_id,
-                    'text': message.text
-                },
-                'regex': pattern
-            })
-            logger.info(f'Message filter match: {log_data}')
+            log_data.update(
+                {
+                    "message": {"id": message.message_id, "text": message.text},
+                    "regex": pattern,
+                }
+            )
+            logger.info(f"Message filter match: {log_data}")
             if message.delete():
-                logger.info(f'Deleted message {message.message_id}')
+                logger.info(f"Deleted message {message.message_id}")
             else:
-                logger.info(f'Could not delete message {message.message_id}')
+                logger.info(f"Could not delete message {message.message_id}")
             break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
