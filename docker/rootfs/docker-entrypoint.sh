06dbd550be1b0cf92135c3e838cd3ffc1f1514fd
Lee Watson <me@lwatson.dev>
2024-02-22 18:08:37 +0000

    Use {} for env vars in scripts

--- docker/rootfs/docker-entrypoint.sh
+++ docker/rootfs/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -eu
 
-if [ -z "$(find "$ROOT_PATH_FOR_DYNACONF" -type f -mindepth 1 -maxdepth 1)" ]; then
-    echo "Copying default settings.yml to $ROOT_PATH_FOR_DYNACONF"
-    cp -au gentoogram/resources/config/settings.yml "$ROOT_PATH_FOR_DYNACONF"
+if [ -z "$(find "${ROOT_PATH_FOR_DYNACONF}" -type f -mindepth 1 -maxdepth 1)" ]; then
+    echo "Copying default settings.yml to ${ROOT_PATH_FOR_DYNACONF}"
+    cp -au "gentoogram/resources/config/settings.yml" "${ROOT_PATH_FOR_DYNACONF}"
 fi
 
 exec python -m gentoogram
